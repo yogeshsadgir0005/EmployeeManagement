d7a7141623fcc9b8de321f2791fe0b894e1b2260
Yogesh Sadgir <yogeshsadgir05@gmail.com>
2026-01-21 23:38:09 +0530

    Update Signup.jsx

--- client/src/pages/auth/Signup.jsx
+++ client/src/pages/auth/Signup.jsx
@@ -9,7 +9,7 @@ const Signup = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/signup', formData);
+      const res = await axios.post('https://employeemanagement-8c1x.onrender.com/api/auth/signup', formData);
       // Navigate to separate OTP page with state
       navigate('/verify-otp', { 
         state: { 
